e75f2f344b1a9f130b092e3bf60ac604441224d8
Daniel <danielcazacu205@gmail.com>
2026-07-24 13:37:57 +0300

    add:monitor

---
+++ Testbench/ALU_monitor.sv
@@ -0,0 +1,46 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class ALU_monitor extends uvm_monitor;
+
+    `uvm_component_utils(ALU_monitor)
+
+    virtual ALU_inf inf;
+
+    function new(string name = "ALU_monitor", uvm_component parent);
+ 
+        super.new(name,parent);
+ 
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+
+        super.build_phase(phase);
+
+        if(!uvm_config_db#(virtual ALU_inf) :: get(this,"*","inf",inf))
+
+            `uvm_fatal("NOVIF","Interface not found")
+      
+    endfunction
+
+    task run_phase(uvm_phase phase);
+
+        super.run_phase(phase);
+
+        ALU_seq_item seq_itm;
+
+        forever begin
+            
+            seq_itm = ALU_seq_item :: type_id :: create("seq_itm",this);
+
+            seq_itm.A = inf.A;
+            seq_itm.B = inf.B;
+            seq_itm.Op = inf.Opcode;
+            seq_itm.Res = inf.Result;
+
+            mon_ap.write(seq_itm);
+        end
+      
+    endtask
+  
+endclass
