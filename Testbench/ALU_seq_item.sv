1953ea48c32c8d3bdbb59298f09fcd4a2aea9206
Daniel <danielcazacu205@gmail.com>
2026-07-24 12:40:16 +0300

    add:seq_item

---
+++ Testbench/ALU_seq_item.sv
@@ -0,0 +1,38 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class ALU_seq_item extends uvm_sequence_item;
+
+    rand logic [3:0] A;
+    rand logic [3:0] B;
+    rand logic [3:0] Op;
+    logic [7:0] Res;
+
+    `uvm_object_utils(Ram_seq_item)
+
+    function new(string name = "Ram_seq_item");
+ 
+        super.new(name);
+ 
+    endfunction
+
+    constraint valid_opcode {
+        Op inside {[4'b0000:4'b1011]};
+    }
+
+    constraint input_dist{
+        
+        A dist {
+            4'h0 := 20,
+            4'hF := 20,
+            [4'h1:4'hE] :/ 60
+        };
+
+        B dist {
+            4'h0 := 20,
+            4'hF := 20,
+            [4'h1:4'hE] :/ 60
+        };
+    }
+  
+endclass
