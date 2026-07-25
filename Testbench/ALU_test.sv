953b11e9b8e1ecaf256b6d6eaa74d008d39babcc
Daniel <danielcazacu205@gmail.com>
2026-07-25 18:55:47 +0300

    add:test

---
+++ Testbench/ALU_test.sv
@@ -0,0 +1,40 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class ALU_test extends uvm_test;
+
+    `uvm_component_utils(ALU_test)
+
+    ALU_env envir;
+
+    ALU_seq seq;
+
+    function new(string name = "ALU_test",uvm_component parent);
+ 
+        super.new(name,parent);
+ 
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+
+        super.build_phase(phase);
+
+        envir = ALU_env :: type_id :: create("envir",this);
+      
+    endfunction
+
+    task run_phase(uvm_phase phase);
+
+        super.run_phase(phase);
+
+        phase.raise_objection(this);
+
+        seq = ALU_seq :: type_id :: create("seq");
+
+        seq.start(envir.agn.seqr);
+
+        phase.drop_objection(this);
+      
+    endtask
+  
+endclass
