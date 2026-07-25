6a88a1af2e3eeb7f9968ab6a845283b41468fe43
Daniel <danielcazacu205@gmail.com>
2026-07-25 19:13:32 +0300

    add:top module

---
+++ Testbench/ALU_top.sv
@@ -0,0 +1,20 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+module ALU_top;
+
+    ALU_inf inf();
+
+    ALU_Design dut(.A(inf.A),
+                   .B(inf.B),
+                   .Opcode(inf.Opcode),
+                   .Result(inf.Result));
+
+    initial begin
+
+        uvm_config_db#(virtual ALU_inf)::set(null, "*", "inf", inf);
+        run_test("ALU_test");
+
+    end
+
+endmodule
