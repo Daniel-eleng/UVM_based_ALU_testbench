62b6f74bfc47f6c664d94b72f170de094486cf59
Daniel <danielcazacu205@gmail.com>
2026-07-25 18:42:28 +0300

    add:coverage

---
+++ Testbench/ALU_coverage.sv
@@ -0,0 +1,46 @@
+
+class ALU_coverage extends uvm_subscriber#(ALU_seq_item);
+
+    `uvm_component_utils(ALU_coverage)
+
+    ALU_seq_item seq_itm;
+
+    function new(string name = "ALU_coverage",uvm_component parent);
+ 
+        super.new(name,parent);
+
+        ALU_cg = new();
+ 
+    endfunction
+
+    covergroup ALU_cg;
+        Op_cp : coverpoint seq_itm.Op{
+            illegal_bins invalid = {[4'b1100:4'b1111]};
+        }
+        A_cp : coverpoint seq_itm.A{
+            bins zero = {0};
+            bins max = {15};
+            bins mid = {[1 : 14]};
+        }
+        B_cp : coverpoint seq_itm.B{
+            bins zero = {0};
+            bins max = {15};
+            bins mid = {[1 : 14]};
+        }
+        all_cross : cross Op_cp , A_cp , B_cp;
+    endgroup
+
+    function void write(ALU_seq_item seq_itm);
+
+        this.seq_itm = seq_itm;
+        ALU_cg.sample();
+      
+    endfunction
+
+    function void report_phase(uvm_phase phase);
+
+        `uvm_info(get_type_name(),$sformatf("Total coverage:%0.2f",ALU_cg.get_coverage()),UVM_LOW)
+      
+    endfunction
+  
+endclass
